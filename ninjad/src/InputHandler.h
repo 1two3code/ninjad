b0665bb515a3a6e0d6c7a5656089669ea2a1cf86
gustavsson.erik.karl@gmail.com <gustavsson.erik.karl@gmail.com@f09618c1-1475-88d2-b3a7-faa968b56df6>
2011-02-17 11:40:17 +0000

    player och InputHandler tillagada(inte kompletta, buggararar)

---
+++ ninjad/src/InputHandler.h
@@ -0,0 +1,31 @@
+//inputklassen kan nu; kolla om man går till höger, vänster, hoppar eller klickar
+//kan även kolla musens position.
+//osäker på variablerna right, left, jump, click och hur man ändar dem dock
+
+#ifndef INPUTHANDLER_H
+#define INPUTHANDLER_H
+#include <SFML\System.hpp>
+#include <SFML\Graphics.hpp>
+#include "Game.h"
+
+class InputHandler
+{
+private:
+	static InputHandler input;
+	InputHandler();
+	~InputHandler();
+/*	sf::Key::Code right;
+	sf::Key::Code left;
+	sf::Key::Code jump;
+	sf::Mouse::Button click;*/
+public:
+	static InputHandler& getInstance();
+	bool isPressRight(RenderWindow* wnd);
+	bool isPressLeft(RenderWindow* wnd);
+	bool isPressJump(RenderWindow* wnd);
+	bool isPressClick(RenderWindow* wnd);
+	int getMousePosX(RenderWindow* wnd);
+	int getMousePosY(RenderWindow* wnd);
+};
+
+#endif
